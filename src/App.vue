9b5ad694e2874f90ed047c005261a48f569b509b
cuiyj <cuiyj>
2020-09-05 14:41:29 +0800

    router对象

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
       <div id="app">
-        <h1>Hello App!</h1>
+        <h1>Hello App!{{route.path}}</h1>
+        <h4>当前路由对象{{route}}</h4>
+        <button @click="goBack">路由历史回退</button>
         <p>
           <!-- 使用 router-link 组件来导航. -->
           <!-- 通过传入 `to` 属性指定链接. -->
           <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-          <router-link to="/foo">Go to Foo</router-link>
-          <br/>
-          <router-link to="/bar">Go to Bar</router-link>
+          <router-link to="/foo">Go to Foo.   </router-link>  
+          <router-link to="/bar">Go to Bar.   </router-link>
         </p>
         <!-- 路由出口 -->
         <!-- 路由匹配到的组件将渲染在这里 -->
@@ -18,10 +19,24 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
+import myrouter from './myrouter'
+
 export default {
   name: 'App',
+  router:myrouter.router,
   components: {
     // HelloWorld
+  },
+  computed: {
+    route() {
+      // 我们很快就会看到 `params` 是什么
+      return this.$route   
+    }
+  },
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
   }
 }
 </script>
