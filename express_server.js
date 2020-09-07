d132997088493603cc655570778ca886213ba077
cuiyj <cuiyj>
2020-09-07 19:29:39 +0800

    node express_server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,4 @@
 var express = require('express');
 var app = express();
 app.use('/myvuerouter',express.static('dist'));
-var server = app.listen(8080, function () { 
-    console.log("- Local: [ http://localhost:8080/myvuerouter ]");
-});
+app.listen(8080, function () {console.log("- Local: [ http://localhost:8080/myvuerouter ]")});
