e83e82d7420e35f72eddbfdf9dd08e1d3a519743
cyj <cyj>
2020-09-09 00:58:46 +0800

    vux

--- src/com_store/Counter.vue
+++ src/com_store/Counter.vue
@@ -1,13 +1,33 @@
 <template>
-      <div>我是Counter:{{com_count}}</div>
+      <div><button @click="cpp">c++</button>
+      我是Counter|count01={{count01}}|countPlusLocalState={{countPlusLocalState}}|
+      countAlias:{{countAlias}}|localComputed={{localComputed}}
+      <br/> doneTodos={{doneTodos}} || doneTodosCount={{doneTodosCount}} 
+      || getTodoById={{getTodoById(2)}}
+      </div>
 </template>
 
 <script> 
+import { mapState,mapGetters } from 'vuex';
 export default {
   name: 'Counter',
+  data:function(){return{localCount:44,getTodoById01:'null'}},
   computed: {
-    com_count () {
-      return this.$store.state.count
+      localComputed(){
+        return this.localCount
+      },
+      ...mapState({ 
+        count01:state => state.count,
+        countAlias: 'count',
+        countPlusLocalState(statearg){
+          return statearg.count+this.localCount
+        }
+      }),
+      ...mapGetters(['doneTodos','doneTodosCount','getTodoById'])
+  },
+  methods:{
+    cpp:function(){
+      this.$store.commit("increment")
     }
   }
 }
--- src/myvuex.js
+++ src/myvuex.js
@@ -5,7 +5,19 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 100
+    count: 100,
+    todos: [ { id: 1, text: '.1..', done: true }, { id: 2, text: '.2..', done: false } ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find(todo => todo.id === id)
+    }
   },
   mutations: {
     increment (state) {
